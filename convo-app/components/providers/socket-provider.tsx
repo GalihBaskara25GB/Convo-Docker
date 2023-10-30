f81c2e8b0da1662b9015ab20a4ecd865a7bff4e3
Galih Baskara <galihbaskara25.gb@gmail.com>
2023-10-30 10:42:30 +0700

    remove websocket

--- convo-app/components/providers/socket-provider.tsx
+++ convo-app/components/providers/socket-provider.tsx
@@ -32,8 +32,7 @@ export const SocketProvider = ({
 
   useEffect(() => {
     const socketInstance = new (ClientIO as any)(process.env.NEXT_PUBLIC_SITE_URL!, {
-      // path: "/api/socket/io",
-      path: "/api/websocket",
+      path: "/api/socket/io",
       addTrailingSlash: false,
     });
 
